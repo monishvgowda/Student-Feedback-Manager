c9b9a25938b8b21e4e0f8de451aa8efa454b9f5c
monishvgowda <147648832+monishvgowda@users.noreply.github.com>
2025-04-09 16:48:34 +0530

    Delete feature directory
    it was wrong

--- feature/score_calculator.py
+++
@@ -1,4 +0,0 @@
-def calculate_average(student_data):
-    # return avg 
-    avgmark = (int(student_data[2]) + int(student_data[3]) + int(student_data[4])) / 3
-    return round(avgmark, 2)  # Round to 2 decimal so no error coe
